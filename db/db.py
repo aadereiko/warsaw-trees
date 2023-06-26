0a4c60e18078602f3fa59df1d5f1001b43973efc
sasha <aadereiko@gmail.com>
2023-06-26 17:41:27 +0200

    fix the problem with host;

--- db/db.py
+++ db/db.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-DB_NAME = 'warsaw-trees'
+DB_NAME = 'warsaw_trees'
 DB_USER = 'aadereiko'
 DB_PASSWORD = '220699'
 DB_PORT = '5432'
@@ -9,7 +9,7 @@ DB_HOST = 'localhost'
 
 def init_db():
     try:
-        conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD, port=DB_PORT)
+        conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD, port=DB_PORT, host=DB_HOST)
     except psycopg2.Error as err:
         print(err)
 
